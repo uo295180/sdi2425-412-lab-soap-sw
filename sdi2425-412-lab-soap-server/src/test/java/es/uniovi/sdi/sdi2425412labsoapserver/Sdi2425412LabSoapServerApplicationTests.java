3707280272feb1be23b0783f5622ada7b2dcdb7a
orvizz <marioorvvie@gmail.com>
2025-04-08 23:08:16 +0200

    SDI-2425-412-SW-11.1-Servicio-SOAP-Server.

---
+++ sdi2425-412-lab-soap-server/src/test/java/es/uniovi/sdi/sdi2425412labsoapserver/Sdi2425412LabSoapServerApplicationTests.java
@@ -0,0 +1,13 @@
+package es.uniovi.sdi.sdi2425412labsoapserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Sdi2425412LabSoapServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
